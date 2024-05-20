f7a85a808dda631f05e974980ea9e76377ac4625
Thiago Leite <thiagofmleite@gmail.com>
2024-05-20 18:43:21 -0300

    Building a filter

--- Modulo01/src/routes.js
+++ Modulo01/src/routes.js
@@ -9,7 +9,15 @@ export const routes = [
     method: "GET",
     path: buildRoutePath("/users"),
     handler: (req, res) => {
-      const users = database.select("users");
+      const { search } = req.query;
+
+      const users = database.select(
+        "users",
+        search && {
+          name: search,
+          email: search,
+        }
+      );
       return res.end(JSON.stringify(users));
     },
   },
