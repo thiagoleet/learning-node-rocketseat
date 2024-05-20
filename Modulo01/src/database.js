49bda6b28ccb36da231341857304da17b2694404
Thiago Leite <thiagofmleite@gmail.com>
2024-05-20 18:35:44 -0300

    Building an API with fake database

---
+++ Modulo01/src/database.js
@@ -0,0 +1,60 @@
+import fs from "node:fs/promises";
+
+const databasePath = new URL("./data/db.json", import.meta.url);
+
+export class Database {
+  #database = {};
+
+  constructor() {
+    fs.readFile(databasePath, "utf-8")
+      .then((data) => {
+        this.#database = JSON.parse(data);
+      })
+      .catch(() => {
+        this.#persist();
+      });
+  }
+
+  #persist() {
+    fs.writeFile(databasePath, JSON.stringify(this.#database));
+  }
+
+  select(table) {
+    const data = this.#database[table] ?? [];
+
+    return data;
+  }
+
+  insert(table, data) {
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data);
+    } else {
+      this.#database[table] = [data];
+    }
+
+    this.#persist();
+    return data;
+  }
+
+  delete(table, id) {
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id);
+
+    if (rowIndex > -1) {
+      this.#database[table].splice(rowIndex, 1);
+      this.#persist();
+    }
+  }
+
+  update(table, id, data) {
+    let response = null;
+    const rowIndex = this.#database[table].findIndex((row) => row.id === id);
+
+    if (rowIndex > -1) {
+      response = { id, ...data };
+      this.#database[table][rowIndex] = response;
+      this.#persist();
+    }
+
+    return response;
+  }
+}
